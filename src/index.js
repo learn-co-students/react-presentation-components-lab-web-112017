6f23e14b1304257ed8592093dca747c8de69d12d
ddbrennan <ddbren@gmail.com>
2018-01-31 15:27:26 -0500

    happy sad stuff happening

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => alert('this is a lie') } />
   </div>,
   document.getElementById('root')
 );
